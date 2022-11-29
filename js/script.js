68a3f721da13a7ae128fffba5623fdb51ffa119c
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-11-30 08:52:56 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.menu-mobile').click(function (event) {
-        $('.menu-mobile, .nav-menu, .nav-menu-list, nav-menu-hidden, menu-list-hidden').toggleClass('active');
+        $('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden').toggleClass('active');
     });
 });
 
