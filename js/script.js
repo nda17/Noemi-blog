ff540a883281824ebe07c084877ab70971398a3b
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-02 08:55:48 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-// document.querySelector('.menu-mobile').onclick = function() {
-//   document.querySelector('.menu-mobile').classList.toggle('active');
-//   document.querySelector('.nav-menu-hidden').classList.toggle('active');
-// };
-
-document.querySelectorAll('.menu-mobile').onclick = function() {
-  document.querySelectorAll('.menu-mobile .nav-menu-hidden').classList.toggle('active');
+document.querySelector('.menu-mobile').onclick = function() {
+  document.querySelector('.menu-mobile').classList.toggle('active');
+  document.querySelector('.nav-menu-hidden').classList.toggle('active');
 };
 
 document.querySelector('.feedback').onclick = function() {
