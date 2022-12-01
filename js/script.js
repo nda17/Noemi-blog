3ae0dccadd7c22b7d0f0d85a464ef07b9a267283
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-02 07:03:13 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.querySelectorAll('.menu-mobile').onclick = function() {
 };
 
 
-
 document.querySelectorAll('.feedback').onclick = function() {
   document.querySelectorAll('.popup-hidden').classList.toggle('active');
 };
@@ -12,88 +11,5 @@ document.querySelectorAll('.popup-close').onclick = function() {
   document.querySelectorAll('.popup-hidden').classList.toggle('active');
 };
 
-
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//     $('.menu-mobile').click(function (event) {
-//         $('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden').toggleClass('active');
-//     });
-// });
-
-// $(document).ready(function () {
-//     $('.feedback').click(function (event) {
-//         $('.popup-hidden').toggleClass('active');
-//     });
-// });
-
-// $(document).ready(function () {
-//     $('.popup-close').click(function (event) {
-//         $('.popup-hidden').removeClass('active');
-//     });
-// });
-
-
-
-// let listMenu- = document.querySelector('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden');
-// let menuMobile = document.querySelectorAll('.menu-mobile');
-
-// document.querySelectorAll(.menuMobile).addListener("click"), (e) => {
-  
-//      listMenu.classList.add('active');
-//      listMenu.classList.remove('active');
-// };
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//     $('.feedback').click(function (event) {
-//         $('.popup-hidden').toggleClass('active');
-//     });
-// });
-
-// $(document).ready(function () {
-//     $('.popup-close').click(function (event) {
-//         $('.popup-hidden').removeClass('active');
-//     });
-// });
-
-
-
-
-
-
-
-
-
-// 
-// let menuMobile = document.querySelector('.menu-mobile');
-
-// menuMobile.onclick = function() {
-//   listMenu.classList.remove('.active');
-//   listMenu.classList.add('.active');
-// };
-
-
-
-
-// let closeButton = document.querySelector('.popup-close')
-//  closeButton.forEach(item => { 
-//         item.addEventListener('click', (e) =>{
-//         closeButton.forEach(el=>{ popup-hidden.classList.remove('activeSSS'); });
-//         item.classList.add('activeSSS');
-//     });
-// });
 
 
