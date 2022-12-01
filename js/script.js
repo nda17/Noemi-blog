b236373b99c574438e41781b962cc4668835a739
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-02 07:11:50 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 document.querySelector('.menu-mobile').onclick = function() {
-  document.querySelector('.menu-mobile').classList.toggle('.active');
+  document.querySelector('.menu-mobile').classList.toggle('active');
 };
 
 
 document.querySelector('.feedback').onclick = function() {
-  document.querySelector('.popup-hidden').classList.toggle('.active');
+  document.querySelector('.popup-hidden').classList.toggle('active');
 };
 
 document.querySelector('.popup-close').onclick = function() {
-  document.querySelector('.popup-hidden').classList.toggle('.active');
+  document.querySelector('.popup-hidden').classList.remove('active');
 };
 
 
