d0c02f1fd180939ea2716352ddf1af75dfaf8fd5
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-02 07:12:31 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,27 +35,4 @@ document.querySelector('.popup-close').onclick = function() {
 // let listMenu- = document.querySelector('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden');
 // let menuMobile = document.querySelectorAll('.menu-mobile');
 
-// document.querySelectorAll(.menuMobile).addListener("click"), (e) => {
-  
-//      listMenu.classList.add('active');
-//      listMenu.classList.remove('active');
-// };
-
-
-
-
-
-// $(document).ready(function () {
-//     $('.feedback').click(function (event) {
-//         $('.popup-hidden').toggleClass('active');
-//     });
-// });
-
-// $(document).ready(function () {
-//     $('.popup-close').click(function (event) {
-//         $('.popup-hidden').removeClass('active');
-//     });
-// });
-
-
 
