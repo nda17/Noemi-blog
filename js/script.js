63542f37f7d8a86169a6a0f93a8a64c1940bf233
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2022-12-02 07:04:52 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,61 @@
-document.querySelectorAll('.menu-mobile').onclick = function() {
-  document.querySelectorAll('.menu-mobile').classList.toggle('active');
+document.querySelector('.menu-mobile').onclick = function() {
+  document.querySelector('.menu-mobile').classList.toggle('active');
 };
 
 
-document.querySelectorAll('.feedback').onclick = function() {
-  document.querySelectorAll('.popup-hidden').classList.toggle('active');
+document.querySelector('.feedback').onclick = function() {
+  document.querySelector('.popup-hidden').classList.toggle('active');
 };
 
-document.querySelectorAll('.popup-close').onclick = function() {
-  document.querySelectorAll('.popup-hidden').classList.toggle('active');
+document.querySelector('.popup-close').onclick = function() {
+  document.querySelector('.popup-hidden').classList.toggle('active');
 };
+
+
+// $(document).ready(function () {
+//     $('.menu-mobile').click(function (event) {
+//         $('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden').toggleClass('active');
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.feedback').click(function (event) {
+//         $('.popup-hidden').toggleClass('active');
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.popup-close').click(function (event) {
+//         $('.popup-hidden').removeClass('active');
+//     });
+// });
+
+
+
+// let listMenu- = document.querySelector('.menu-mobile, .nav-menu, .nav-menu-list, .nav-menu-hidden, .menu-list-hidden');
+// let menuMobile = document.querySelectorAll('.menu-mobile');
+
+// document.querySelectorAll(.menuMobile).addListener("click"), (e) => {
+  
+//      listMenu.classList.add('active');
+//      listMenu.classList.remove('active');
+// };
+
+
+
+
+
+// $(document).ready(function () {
+//     $('.feedback').click(function (event) {
+//         $('.popup-hidden').toggleClass('active');
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.popup-close').click(function (event) {
+//         $('.popup-hidden').removeClass('active');
+//     });
+// });
 
 
 
