d793dd4607350d76cffb774a6dd6ce5d44b41309
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2023-02-07 07:49:09 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-document.querySelector('.menu-mobile').onclick = function() {
-  document.querySelector('.menu-mobile').classList.toggle('active');
-  document.querySelector('.nav-menu-hidden').classList.toggle('active');
+const menuMobile = document.querySelector('.menu-mobile');
+const mobileMenuHidden = document.querySelector('.nav=menu-hidden');
+
+function showMobileMenu() {
+  menuMobile.classList.toggle('active');
+  mobileMenuHidden.classList.toggle('active');
 };
 
-document.querySelector('.feedback').onclick = function() {
-  document.querySelector('.popup-hidden').classList.toggle('active');
-};
+menuMobile.addEventListener('click', showMobileMenu);
 
-document.querySelector('.feedback-mobile-link').onclick = function() {
-  document.querySelector('.popup-hidden').classList.toggle('active');
-};
 
-document.querySelector('.popup-close').onclick = function() {
-  document.querySelector('.popup-hidden').classList.toggle('active');
-};
+
+
+
+
+// document.querySelector('.menu-mobile').onclick = function() {
+//   document.querySelector('.menu-mobile').classList.toggle('active');
+//   document.querySelector('.nav-menu-hidden').classList.toggle('active');
+// };
+
+// document.querySelector('.feedback').onclick = function() {
+//   document.querySelector('.popup-hidden').classList.toggle('active');
+// };
+
+// document.querySelector('.feedback-mobile-link').onclick = function() {
+//   document.querySelector('.popup-hidden').classList.toggle('active');
+// };
+
+// document.querySelector('.popup-close').onclick = function() {
+//   document.querySelector('.popup-hidden').classList.toggle('active');
+// };
 
 
 
